735d88d67b3dba3f2c8dc3d19ecfe96c37ad6569
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2025-03-24 01:16:42 -0300

    falta varios ajuste

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,15 +1,7 @@
-const menuHamburguer= document.querySelector('.menu-hamburguer')
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const nav = document.querySelector('.nav');
+
 menuHamburguer.addEventListener('click', () => {
-   toggleMenu()
-})
-function toggleMenu(){
-  const nav = document.querySelector('.nav-responsive')
   menuHamburguer.classList.toggle('change');
-
-
-  if (menuHamburguer.classList.contains('change')){
-    nav.style.display = 'block'; 
-   }else{
-      nav.style.display = 'none'
-   }
-}
+  nav.classList.toggle('active');
+});
